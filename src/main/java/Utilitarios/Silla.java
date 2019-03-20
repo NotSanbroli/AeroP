ff01a5381459d065228b98ff3d48a7893523cd28
fetec <fetec@ElMero-Maquio>
2019-03-20 11:43:14 -0500

    SE suben primeras clases basicas

---
+++ src/main/java/Utilitarios/Silla.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilitarios;
+
+/**
+ *
+ * @author fetec
+ */
+public class Silla {
+    
+    private double valor=100;
+    private boolean vip=false;
+    private boolean comprada=false;
+    private String nombre;
+    private Persona persona;
+    
+    
+}
