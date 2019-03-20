2564061da5776528025f3b863aac78d1222d1532
fetec <fetec@ElMero-Maquio>
2019-03-20 11:51:10 -0500

    SE suben primeras clases basicas

--- src/main/java/Utilitarios/Silla.java
+++ src/main/java/Utilitarios/Silla.java
@@ -16,6 +16,50 @@ public class Silla {
     private boolean comprada=false;
     private String nombre;
     private Persona persona;
+
+    public Silla(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+
+    public boolean isComprada() {
+        return comprada;
+    }
+
+    public void setComprada(boolean comprada) {
+        this.comprada = comprada;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
     
     
 }
