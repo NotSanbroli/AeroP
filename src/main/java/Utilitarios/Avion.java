b88f3d423463d45d491cf55565669ecae954d349
NotSanbroli <47396482+NotSanbroli@users.noreply.github.com>
2019-03-21 15:48:30 -0500

    Imprime aviones y ocupa sillas

---
+++ src/main/java/Utilitarios/Avion.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilitarios;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author fetec
+ */
+public abstract class Avion {
+    
+    private String nombre;
+    HashMap<String,Silla> sillas;
+    private boolean volando=false;
+
+    public Avion(String nombre) {
+        this.nombre = nombre;
+        sillas=new HashMap();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public HashMap<String, Silla> getSillas() {
+        return sillas;
+    }
+
+    public void setSillas(HashMap<String, Silla> sillas) {
+        this.sillas = sillas;
+    }
+
+    public boolean isVolando() {
+        return volando;
+    }
+
+    public void setVolando(boolean volando) {
+        this.volando = volando;
+    }
+    
+    
+}
---
+++ src/main/java/Utilitarios/Cliente.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilitarios;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Darkgrey93
+ */
+public class Cliente {
+    private String nombre;
+    private Integer cedula;
+    private SimpleDateFormat  fechaNacimiento;
+
+    public Cliente(String nombre, int cedula, SimpleDateFormat  fechaNacimiento) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+}
